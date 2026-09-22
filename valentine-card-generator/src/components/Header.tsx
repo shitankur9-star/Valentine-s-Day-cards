048abdbdcaad63fb0803738a60f14c38cdd679e4
shitankur9-star <shitankur9@gmail.com>
2026-09-22 10:48:44 +0530

    Delete valentine-card-generator directory

--- valentine-card-generator/src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-import { Heart } from "lucide-react";
-
-export default function Header() {
-  return <header className="header">
-    <div className="brand"><Heart fill="currentColor" size={22} /> Valentine Card Maker</div>
-    <nav><a href="#create">Create Card</a><a href="#templates">Templates</a><a href="#about">About</a></nav>
-  </header>;
-}
